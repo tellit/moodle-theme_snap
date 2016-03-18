15f45e542def17c494bea3a6a3d65ecca2bb43d6
David Aylmer <davidaylmer@gmail.com>
2016-03-18 16:11:55 +0000

    WIP

--- layout/footer.php
+++ layout/footer.php
@@ -96,6 +96,8 @@ if (empty($PAGE->theme->settings->copyrightnotice)) {
     echo $PAGE->theme->settings->copyrightnotice;
 }
 
+//<a class="next_section" href="#section-1"><div class="nav_icon"><i class="icon-arrow-right"></i></div><span class="text"><span class="nav_guide">Next section</span><br>Week 1</span></a>
+
 $showcompletionmodal = false;
 
 //1 If we are on a mod page...
@@ -103,28 +105,27 @@ $pagepath = explode('-', $PAGE->pagetype);
 if ($pagepath[0] == 'mod') {
 
     //2 Check course completion setting
-    //Ask Leonard: Does course completion have to be set for activity completion to work
     if ($COURSE->enablecompletion == COMPLETION_ENABLED) {
-        
-    }
     
-    //3 Check completion setting of current mod
-    // Don't bother popping a modal if completion is based on user clicking a box ()COMPLETION_TRACKING_MANUAL)
-    if ($PAGE->cm->completion == COMPLETION_TRACKING_AUTOMATIC) {
-             
-        //4 Check completion of current mod
-        $completion = $DB->get_record('course_modules_completion', array('coursemoduleid'=>$PAGE->cm->id, 'userid'=>$USER->id));
-                                       
-        if (!empty($completion)) {
-            if (!empty($completion->timemodified)) {
-                
-                if (abs(time() - $completion->timemodified) < 20000) {
-                    if ($completion->completionstate == COMPLETION_COMPLETE || $completion->completionstate == COMPLETION_COMPLETE_PASS) {
-                        $showcompletionmodal = true;
-                    }
-                }     
-            }
-        }  
+        //3 Check completion setting of current mod
+        // Don't bother popping a modal if completion is based on user clicking a box (COMPLETION_TRACKING_MANUAL)
+        if ($PAGE->cm->completion == COMPLETION_TRACKING_AUTOMATIC) {
+                 
+            //4 Check completion of current mod
+            $completion = $DB->get_record('course_modules_completion', array('coursemoduleid'=>$PAGE->cm->id, 'userid'=>$USER->id));
+            
+                                           
+            if (!empty($completion)) {
+                if (!empty($completion->timemodified)) {
+                    
+                    if (abs(time() - $completion->timemodified) < 20000) {
+                        if ($completion->completionstate == COMPLETION_COMPLETE || $completion->completionstate == COMPLETION_COMPLETE_PASS) {
+                            $showcompletionmodal = true;
+                        }
+                    }     
+                }
+            }  
+        }
     }      
 }    
 
@@ -165,33 +166,40 @@ if ($showcompletionmodal) { //}($PAGE->theme->settings->activitycompletionmodal)
 
 //$link = html_writer::link($url, get_string('editcustomfooter', 'theme_snap'), ['class' => 'btn btn-default btn-sm']);
 $nextmodurl =  $nextmod->url->out();
-echo ' <!-- Modal -->
-  <div class="modal fade activitycompletemodal" id="activitycompletemodal" role="dialog">
+echo '<!-- Modal -->
+<div class="modal fade activitycompletemodal" id="activitycompletemodal" role="dialog">
     <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content activitycompletemodal-content">
-        <div class="modal-header activitycompletemodal-header">
-          <button type="button" class="close activitycompletemodal-close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title activitycompletemodal-title"><span class="glyphicon glyphicon-star activitycompletemodal-star"></span> Activity Complete!</h4>
+        <!-- Modal content-->
+        <div class="modal-content activitycompletemodal-content">
+            <div class="modal-header activitycompletemodal-header">
+                <span class="glyphicon glyphicon-ok activitycompletemodal-ok"></span>
+            </div>
+            <div class="modal-body activitycompletemodal-body">
+                <h4 class="modal-title activitycompletemodal-title">
+                    Activity Complete
+                </h4>
+                <p>You completed the activity <span class="activitycompletecurrentmodname">' . $PAGE->cm->name . '</span> 
+                    <span class="activitycompletecurrentmodtype">(' . $PAGE->cm->modname . ')</span> 
+                    and have released the next activity. <span class="activitycompletenextmodname">' . $nextmod->name . '</span>
+                </p>
+            </div>
+            <div class="modal-footer activitycompletemodal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <a href="' . $nextmodurl . '" class="activitycompletenextmodlink">Next activity
+                <span class="activitycompletenextmodname">' . $nextmod->name . '</span> 
+                <span class="activitycompletecurrentmodtype">(' . $nextmod->modname . ')</span>
+                <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+                </a>
+            </div>
         </div>
-        <div class="modal-body activitycompletemodal-body">
-          <p>Congratulations on completing <span class="activitycompletecurrentmodname">' . $PAGE->cm->name . '</span> <span class="activitycompletecurrentmodtype">(' . $PAGE->cm->modname . ')</span></p>
-          <p><a href="' . $nextmodurl . '" class="activitycompletenextmodlink">Continue to next activity: <span class="activitycompletenextmodname">' . $nextmod->name . '</span> <span class="activitycompletecurrentmodtype">(' . $nextmod->modname . ')</span></a></p>
-        </div>
-        <div class="modal-footer activitycompletemodal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-      
     </div>
-  </div> <script type="text/javascript">
+</div>
+<script type="text/javascript">
     $(window).load(function(){
         //$(\'#myModal\').modal(\'show\');
         setTimeout(function(){$(\'#activitycompletemodal\').modal(\'show\');}, 2000);
     });
-</script>
-';
+</script>';
 }
 
 ?>
