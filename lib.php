61d9016e59f253d124f203aafc412f5538c65bb3
David Aylmer <davidaylmer@gmail.com>
2017-05-16 15:27:23 +0100

    re-applied coursepageredirect

--- lib.php
+++ lib.php
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+require_once __DIR__ . '/coursepageredirect.php';
+
 /**
  * Standard library functions for snap theme.
  *
